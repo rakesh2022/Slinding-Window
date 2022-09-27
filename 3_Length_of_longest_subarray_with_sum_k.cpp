c64e3dd824831206d2fb63423e11d08c2a03c53b
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-27 17:48:32 +0530

    first commit

---
+++ 3_Length_of_longest_subarray_with_sum_k.cpp
@@ -0,0 +1,23 @@
+https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+
+class Solution{
+    public:
+    int lenOfLongSubarr(int A[],  int N, int K) 
+    { 
+        unordered_map<int, int>mp;
+        int i=-1,j=0;
+        int sum=0,ans=0;
+        mp[0]=-1;
+        while(j<N){
+            sum+=A[j];
+            if(mp.find(sum-K)!=mp.end()){
+                ans=max(ans,j-mp[sum-K]);
+            }
+            if(mp.find(sum)==mp.end())
+            mp[sum]=j;
+            j++;
+        }
+        return ans;
+    } 
+
+};
