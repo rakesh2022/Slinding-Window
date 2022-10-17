e54ab97759baaa7a20134fea1b98d8cb09b8984a
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-17 11:33:02 +0530

    list of questions

--- 0000__some_impo_ques;.cpp
+++ 0000__some_impo_ques;.cpp
@@ -1,1 +1,20 @@
 https://leetcode.com/problems/frequency-of-the-most-frequent-element/discuss/1175088/C%2B%2B-Maximum-Sliding-Window-Cheatsheet-Template!
+
+
+//  https://leetcode.com/contest/weekly-contest-315/problems/count-subarrays-with-fixed-bounds/
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int i=0,j=0,jmin=-1,jmax=-1,jbad=-1;
+        long long ans=0;
+        for(;j<nums.size();j++){
+            if(nums[j]< minK || nums[j]>maxK)jbad=j;
+            if(nums[j]==minK)jmin=j;
+            if(nums[j]==maxK)jmax=j;
+         
+           ans+= max(0, min(jmin,jmax)-jbad);
+        }
+        return ans;
+    }
+};
